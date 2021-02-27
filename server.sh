b4bbd76980e0576084fe9fe4dd1f0b2897f0cfac
Hunter Wittenborn <git@hunterwittenborn.me>
2021-02-26 19:21:12 -0600

    Delete server.sh

--- server.sh
+++
@@ -1,7 +0,0 @@
-if [[ $1 == "pull" ]]; then
-  rsync -r --progress 192.168.0.124:/var/www/www.hunterwittenborn.me ./
-elif  [[ $1 == "push" ]]; then
-  rsync -r --progress ./ 192.168.0.124:/var/www/www.hunterwittenborn.me
-else
-  echo "[!] Invalid option(s) provided. Please try again with either 'push' or 'pull'"
-fi
